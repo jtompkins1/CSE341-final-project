2147e0da246633106f7949af5363c4b197a080e2
LarrySon786 <richardsbrandon3@gmail.com>
2026-01-31 14:48:32 -0500

    MongoDB-connection completed. I also created one test route to make sure the connection happens without any issues. See index routes '/students' for test route.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
+const studentsController = require('../controllers/studentsController');
 const router = require('express').Router();
+
 
 router.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+
+
+// BRANDON's EXAMPLE ROUTE TO DEMONSTRATE 
+// THAT THE MONGODB DATABASE IS CONNECTED:
+
+router.get('/students', studentsController.getAllStudents);
 
 module.exports = router;
