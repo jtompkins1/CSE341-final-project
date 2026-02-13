6855851339c765a5d938b17f7b15766019ad094b
Arthur Leung <1808arthur@gmail.com>
2026-02-12 22:34:38 -0400

    Updated OAuth

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,23 @@
-const studentsController = require('../controllers/studentsController');
 const router = require('express').Router();
+const passport = require('passport');
 
 router.use('/', require('./swagger'));
 
 router.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out");
+});
+
+// Login & Logout
+router.get('/login', passport.authenticate('github'));
+
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
 });
 
 router.use('/teachers', require('./teachers'));
 router.use('/students', require('./students'));
-// router.use('/', require('./'));
-// router.use('/', require('./'));
 
 module.exports = router;
--- routes/teachers.js
+++ routes/teachers.js
@@ -3,19 +3,32 @@ const router = express.Router();
 
 const usersController = require('../controllers/teachers.js');
 const validator = require('../middleware/validator.js');
+const { isAuthenticated } = require("../middleware/authenticate.js"); 
 
+// GET 
 router.get('/', usersController.getAll);
+router.get('/:id', usersController.getSingle);
+router.get('/classtaught/:id', usersController.getByClassTaught);
+router.get('/subject/:id', usersController.getBySubject);
 
-router.get('/:id',  usersController.getSingle);
 
-router.get('/classtaught/:id',  usersController.getByClassTaught);
+router.post('/', 
+    isAuthenticated, 
+    validator.teacherRules(), 
+    validator.validate, 
+    usersController.addTeacher
+);
 
-router.get('/subject/:id',  usersController.getBySubject);
+router.put('/:id', 
+    isAuthenticated, 
+    validator.teacherRules(), 
+    validator.validate, 
+    usersController.updateTeacher
+);
 
-router.post('/', validator.teacherRules(), validator.validate, usersController.addTeacher);
-
-router.put('/:id', validator.teacherRules(), validator.validate, usersController.updateTeacher);
-
-router.delete('/:id', usersController.deleteTeacher);
+router.delete('/:id', 
+    isAuthenticated, 
+    usersController.deleteTeacher
+);
 
 module.exports = router;
